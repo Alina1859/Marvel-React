fb450c903f60bfe5ef2b735663c53a25a4e658a0
Alina Sultanova <sultanova1859ts@gmail.com>
2023-08-24 11:07:41 +0500

    added github button

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,6 +6,8 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
 import decoration from "../../resources/img/vision.png";
+import gitHubIcon from "../../resources/img/github-logo.svg";
+
 
 class App extends Component {
 
@@ -35,6 +37,14 @@ class App extends Component {
               <CharInfo charId={this.state.selectedChar}/>
             </ErrorBoundary>
           </div>
+          <a 
+            className="gitHub-logo" 
+            href="https://github.com/Alina1859/Marvel-React" 
+            target="_blank" 
+            rel="noreferrer"><img src={gitHubIcon} 
+            width="130" height="50" 
+            alt="Link on project on Github"/>
+          </a>
           <img className="bg-decoration" src={decoration} alt="vision" />
         </main>
       </div>
